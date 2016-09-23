cb0fe754d2576d63e5204b3de7ab26abd51b6518
qhonore <qhonore@student.42.fr>
2016-09-23 08:03:10 +0200

    norme

--- src/print_base.c
+++ src/print_base.c
@@ -6,13 +6,31 @@
 /*   By: qhonore <qhonore@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 03:25:32 by qhonore           #+#    #+#             */
-/*   Updated: 2016/09/23 04:43:11 by qhonore          ###   ########.fr       */
+/*   Updated: 2016/09/23 07:17:05 by qhonore          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		print_ptr(va_list *args, t_param *p)
+static char	*get_arg(va_list *args, t_param *p, int base, int upper)
+{
+	if (p->lgt == 'l')
+		return (ft_ultoa_base(va_arg(*args, unsigned long), base, upper));
+	else if (p->lgt == 'L')
+		return (ft_ulltoa_base(va_arg(*args, unsigned long long), base, upper));
+	else if (p->lgt == 'h')
+		return (ft_usitoa_base(va_arg(*args, unsigned int), base, upper));
+	else if (p->lgt == 'H')
+		return (ft_uctoa_base(va_arg(*args, unsigned int), base, upper));
+	else if (p->lgt == 'j')
+		return (ft_uimtoa_base(va_arg(*args, uintmax_t), base, upper));
+	else if (p->lgt == 'z')
+		return (ft_stoa_base(va_arg(*args, size_t), base, upper));
+	else
+		return (ft_uitoa_base(va_arg(*args, unsigned int), base, upper));
+}
+
+int			print_ptr(va_list *args, t_param *p)
 {
 	char	*str;
 	int		ret;
@@ -28,26 +46,13 @@ int		print_ptr(va_list *args, t_param *p)
 	return (ret);
 }
 
-int		print_hexa_uint(va_list *args, int upper, t_param *p)
+int			print_hexa_uint(va_list *args, int upper, t_param *p)
 {
 	char	*str;
 	int		ret;
 
 	ret = 0;
-	if (p->lgt == 'l')
-		str = ft_ultoa_base(va_arg(*args, unsigned long), 16, upper);
-	else if (p->lgt == 'L')
-		str = ft_ulltoa_base(va_arg(*args, unsigned long long), 16, upper);
-	else if (p->lgt == 'h')
-		str = ft_usitoa_base(va_arg(*args, unsigned int), 16, upper);
-	else if (p->lgt == 'H')
-		str = ft_uctoa_base(va_arg(*args, unsigned int), 16, upper);
-	else if (p->lgt == 'j')
-		str = ft_uimtoa_base(va_arg(*args, uintmax_t), 16, upper);
-	else if (p->lgt == 'z')
-		str = ft_stoa_base(va_arg(*args, size_t), 16, upper);
-	else
-		str = ft_uitoa_base(va_arg(*args, unsigned int), 16, upper);
+	str = get_arg(args, p, 16, upper);
 	if (!p->flag[MIN])
 		ret += put_blank(ft_strlen(str), p) + ft_strlen(str);
 	if (p->flag[SHARP] && *str != '0')
@@ -59,26 +64,13 @@ int		print_hexa_uint(va_list *args, int upper, t_param *p)
 	return (ret);
 }
 
-int		print_octal_uint(va_list *args, t_param *p)
+int			print_octal_uint(va_list *args, t_param *p)
 {
 	char	*str;
 	int		ret;
 
 	ret = 0;
-	if (p->lgt == 'l')
-		str = ft_ultoa_base(va_arg(*args, unsigned long), 8, 0);
-	else if (p->lgt == 'L')
-		str = ft_ulltoa_base(va_arg(*args, unsigned long long), 8, 0);
-	else if (p->lgt == 'h')
-		str = ft_usitoa_base(va_arg(*args, unsigned int), 8, 0);
-	else if (p->lgt == 'H')
-		str = ft_uctoa_base(va_arg(*args, unsigned int), 8, 0);
-	else if (p->lgt == 'j')
-		str = ft_uimtoa_base(va_arg(*args, uintmax_t), 8, 0);
-	else if (p->lgt == 'z')
-		str = ft_stoa_base(va_arg(*args, size_t), 8, 0);
-	else
-		str = ft_uitoa_base(va_arg(*args, unsigned int), 8, 0);
+	str = get_arg(args, p, 8, 0);
 	if (!p->flag[MIN])
 		ret += put_blank(ft_strlen(str), p) + ft_strlen(str);
 	if (p->flag[SHARP] && *str != '0')
--- src/print_int.c
+++ src/print_int.c
@@ -6,32 +6,37 @@
 /*   By: qhonore <qhonore@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 02:39:37 by qhonore           #+#    #+#             */
-/*   Updated: 2016/09/23 04:49:15 by qhonore          ###   ########.fr       */
+/*   Updated: 2016/09/23 07:11:45 by qhonore          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		print_int(va_list *args, t_param *p)
+static char	*get_arg(va_list *args, t_param *p)
+{
+	if (p->lgt == 'l')
+		return (ft_ltoa(va_arg(*args, long)));
+	else if (p->lgt == 'L')
+		return (ft_lltoa(va_arg(*args, long long)));
+	else if (p->lgt == 'h')
+		return (ft_sitoa(va_arg(*args, int)));
+	else if (p->lgt == 'H')
+		return (ft_sctoa(va_arg(*args, int)));
+	else if (p->lgt == 'j')
+		return (ft_imtoa(va_arg(*args, intmax_t)));
+	else if (p->lgt == 'z')
+		return (ft_stoa(va_arg(*args, size_t)));
+	else
+		return (ft_itoa(va_arg(*args, int)));
+}
+
+int			print_int(va_list *args, t_param *p)
 {
 	char	*str;
 	char	ret;
 
 	ret = 0;
-	if (p->lgt == 'l')
-		str = ft_ltoa(va_arg(*args, long));
-	else if (p->lgt == 'L')
-		str = ft_lltoa(va_arg(*args, long long));
-	else if (p->lgt == 'h')
-		str = ft_sitoa(va_arg(*args, int));
-	else if (p->lgt == 'H')
-		str = ft_sctoa(va_arg(*args, int));
-	else if (p->lgt == 'j')
-		str = ft_imtoa(va_arg(*args, intmax_t));
-	else if (p->lgt == 'z')
-		str = ft_stoa(va_arg(*args, size_t));
-	else
-		str = ft_itoa(va_arg(*args, int));
+	str = get_arg(args, p);
 	if (p->flag[PLUS] && *str != '-')
 		str = strjoin_n_free("+", str);
 	p->neg = (*str == '-' ? 1 : 0);
@@ -41,7 +46,8 @@ int		print_int(va_list *args, t_param *p)
 		ft_putchar(*str);
 	if (!p->flag[MIN])
 		ret += put_blank(ft_strlen(str), p) + ft_strlen(str);
-	(p->neg || p->flag[PLUS]) && p->flag[ZERO] ? ft_putstr(str + 1) : ft_putstr(str);
+	(p->neg || p->flag[PLUS]) && p->flag[ZERO] ?
+		ft_putstr(str + 1) : ft_putstr(str);
 	if (p->flag[MIN])
 		ret += put_blank(ft_strlen(str), p) + ft_strlen(str);
 	free(str);
